a46924cf6ca07bd7b631570a0fa0f7d6469b1d83
marchi-thiago <marchi.thiago@gmail.com>
2026-09-09 09:10:49 -0300

    chore: atualiza arquivos do projeto

--- src/components/landing/ConsultoriaLPTemplate.tsx
+++ src/components/landing/ConsultoriaLPTemplate.tsx
@@ -707,7 +707,7 @@ export function ConsultoriaLPTemplate({
                   direto no seu e-mail.
                 </p>
               </div>
-              <NewsletterForm origem={newsletterOrigem} compact variant="light" />
+              <NewsletterForm origem={newsletterOrigem} compact />
             </div>
           </div>
         </div>
